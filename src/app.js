cba728414d71b4f50a409875f842031caccb7526
Pozenatos <luishenrique.n.c.pozenato@gmail.com>
2026-03-12 22:10:27 -0300

    Refatorando listagem de usuários

--- src/app.js
+++ src/app.js
@@ -7,6 +7,14 @@ app.get("/", (request, response) => {
     response.send("The cocain is not good for yoooooooouuuuuu!")
 })
 
+app.use(express.json())
+
+app.post("/users", (req, res) => {
+    console.log(req.body)
+
+    res.send("Estamos trabalhando nisso. Obrigado.")
+})
+
 app.get("/users", userController.getAllUsers)
 
 module.exports = app
